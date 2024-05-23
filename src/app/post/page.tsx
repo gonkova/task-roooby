48c8328cc01141458c3b90f19b93dff8f39a9d47
Boryana Gonkova <36599664+gonkova@users.noreply.github.com>
2024-05-23 10:55:03 +0300

    Update page.tsx

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,10 +1,15 @@
 'use client';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAppContext } from '@/context';
 import PostList from '@/components/PostList';
 
 export default function UsersPage() {
     const { posts, photos, currentPage, totalPages, handleNextPage, handlePreviousPage } = useAppContext();
+
+    useEffect(() => {
+     
+        handleNextPage();
+    }, []);
 
     return (
         <PostList
